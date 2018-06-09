bc617f302aab03da5b885452fa9d7b59facefb91
Victor Nagy <victor.nagy@iusinnovation.se>
2018-06-09 16:12:58 +0200

    Initial

---
+++ app.js
@@ -0,0 +1,5 @@
+let Hall = require('./lib/Hall');
+let Aws = require('./lib/Aws');
+let Mpu = require('./lib/MPU');
+let Vibration = require('./lib/Vibration');
+let Collector = require('./lib/Collector');
